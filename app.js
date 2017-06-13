0c82e151ba312c7b291e0b933cd9d54daf8bce9c
Eduardo Vedes <eduardo.vedes@gmail.com>
2017-06-13 22:42:41 +0100

    update

---
+++ app.js
@@ -0,0 +1,15 @@
+//Requirements
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const port = process.env.PORT || 8080
+
+
+app.use('/', express.static(path.join(__dirname,'./public/')))
+
+
+
+app.listen(port, ()=>{
+    console.log('Server listening on PORT: ' + port)
+})
