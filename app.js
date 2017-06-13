56fc69b0425e609aedef95ef9910360b5639258f
System <none>
2017-06-13 22:05:39 +0000

    🍬🎑 Updated with Glitch

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+//Server Listening Port definition
 const port = process.env.PORT || 8080
 
 
@@ -10,6 +11,7 @@ app.use('/', express.static(path.join(__dirname,'./public/')))
 
 
 
+//Server Listening
 app.listen(port, ()=>{
     console.log('Server listening on PORT: ' + port)
 })
